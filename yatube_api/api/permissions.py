41968abde61e6f6da3766f322d02898c4ca28f3b
bondarval <bondamaker@gmail.com>
2021-10-01 13:59:14 +0500

    still revised third version of sprint 9 homework

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -4,7 +4,5 @@ from rest_framework import permissions
 class AuthorOrReadOnly(permissions.BasePermission):
 
     def has_object_permission(self, view, request, obj):
-        return (
-            request.action in (permissions.SAFE_METHODS, 'retrieve')
-            or obj.author == view.user
-               )
+        return (request.action in (permissions.SAFE_METHODS, 'retrieve')
+                or obj.author == view.user)
