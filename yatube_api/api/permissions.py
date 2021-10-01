e9c9d97fe148a9bfea9f7c807843d7ca4595495e
bondarval <bondamaker@gmail.com>
2021-10-01 13:49:05 +0500

    revised third version of 9 sprint homework

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -5,6 +5,6 @@ class AuthorOrReadOnly(permissions.BasePermission):
 
     def has_object_permission(self, view, request, obj):
         return (
-                request.action in (permissions.SAFE_METHODS, 'retrieve')
-                or obj.author == view.user
-        )
+            request.action in (permissions.SAFE_METHODS, 'retrieve')
+            or obj.author == view.user
+            )
